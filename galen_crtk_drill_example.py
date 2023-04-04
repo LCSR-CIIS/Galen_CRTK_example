d47d41319e3b361682d3edc1b5dfc538aa55fe7c
Hisashi Isihda <hishida3@jh.edu>
2023-04-04 00:12:16 -0400

    Adding new commands

---
+++ galen_crtk_drill_example.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+# Author: Hisashi Ishida
+# Date: 2023-03-31
+#
+# Copyright (c) 2018-2023 Johns Hopkins University, University of Washington, Worcester Polytechnic Institute
+# Released under MIT License
+
+# To communicate with the Galen using ROS topics, run following command:
+# > rosrun galen_crtk_telop_example.py /REMS/Research
+
+import crtk
+import math
+import sys
+import rospy
+import numpy
+import PyKDL
+
+if sys.version_info.major < 3:
+    input = raw_input
+
+# example of application using device.py
+class galen_crtk_teleop_example:
+
+    # configuration
+    def configure(self, device_namespace):
+        # ROS initialization
+        if not rospy.get_node_uri():
+            rospy.init_node('galen_crtk_teleop_example', anonymous = True, log_level = rospy.WARN)
+
+        print(rospy.get_caller_id() + ' -> configuring crtk_device_test for: ' + device_namespace)
+
+        # populate this class with all the ROS topics we need
+        self.crtk_utils = crtk.utils(self, device_namespace)
+        self.crtk_utils.add_operating_state()
+        self.crtk_utils.add_measured_cp()
+        self.crtk_utils.add_measured_cf()
+        self.crtk_utils.add_move_cp()
+
+        #############################
+        # Parameters
+        #############################
+        self.rate = 500    # aiming for 200 Hz
+
+        # Define Start position
+        self.start_cp = PyKDL.Frame()
+        self.start_cp.p = self.measured_cp().p # Vector()
+        self.start_cp.M = self.measured_cp().M # self.start_cp.M.DoRotY(math.pi).DoRotX()
+
+        # for one trajectory
+        self.duration = 5 # 5 seconds
+        self.drill_motion= Vector(40., 0., 0.) # Move 4 cm along x-axis
+        self.z_motion= Vector(0., 0., -2.0) # Move - 0.2 cm along z-axis
+
+        # Transition to next motion
+        self.next_motion = Vector(-40., 20., 0.) # Move -4 cm along x-axis and 2 cm along y axis
+        self.samples = self.duration * self.rate
+
+        self.num_stroke = 4
+
+    # main loop
+    def run(self):
+        # Not yet implemented for Galen robot
+        # if not self.enable(60):
+        #     print("Unable to enable the device, make sure it is connected.")
+        #     return
+
+        self.running = True
+        while (self.running):
+            print ('\n- q: quit\n- p: print position of the robot\n- m: command robot to move(~60s)')
+            answer = input('Enter your choice and [enter] to continue\n')
+            if answer == 'q':
+                self.running = False
+            elif answer == 'p':
+                self.run_print()
+            elif answer == 'm':
+                self.run_move_cp()
+            else:
+                print('Invalid choice\n')
+
+    # print positions
+    def run_print(self):
+        print("Galen robot position")
+        print(self.measured_cp().p)
+
+        # You can add more if you want
+        # print (self.measured_cf())
+
+    # position based teleop
+    def run_move_cp(self):
+
+        goal_cp = PyKDL.Frame()
+        goal_cp.p = self.start_cp.p
+        goal_cp.M = self.start_cp.M # The robot will maintatin the rotation
+        
+        for stroke in range(num_stroke):
+            # Start Drilling
+            goal_cp.p = goal_cp.p + self.z_motion
+            self.move_cp(goal).wait()
+            
+            # loop
+            for i in range(self.samples):
+                goal_cp.p = goal_cp.p + i/self.samples * self.drill_motion
+                self.move_cp(goal)
+                rospy.sleep(1.0/self.rate)
+
+            # Ready for next motion
+            goal_cp.p = goal_cp.p - self.z_motion
+            self.move_cp(goal).wait()
+
+            goal_cp.p = goal_cp.p + self.next_motion
+            self.move_cp(goal).wait()
+
+            handle.wait() # wait 2 seconding for each motion
+            
+
+# use the class now, i.e. main program
+if __name__ == '__main__':
+    try:
+        if (len(sys.argv) != 2):
+            print(sys.argv[0], ' requires one argument, i.e. crtk device namespace')
+        else:
+            example = galen_crtk_teleop_example()
+            example.configure(sys.argv[1])
+            example.run_move_cp()
+
+    except rospy.ROSInterruptException:
+        pass
--- galen_crtk_servo_cv_example.py
+++ galen_crtk_servo_cv_example.py
@@ -16,56 +16,41 @@ import crtk
 import math
 import sys
 import rospy
-import numpy
+import numpy as np
 import PyKDL
 
 
 # example of application using device.py
-class crtk_move_cp_example:
+class crtk_servo_cv_example:
 
     # configuration
     def configure(self, device_namespace):
         # ROS initialization
         if not rospy.get_node_uri():
-            rospy.init_node('crtk_move_cp_example', anonymous = True, log_level = rospy.WARN)
+            rospy.init_node('crtk_servo_cv_example', anonymous = True, log_level = rospy.WARN)
 
         print(rospy.get_caller_id() + ' -> configuring crtk_device_test for: ' + device_namespace)
         # populate this class with all the ROS topics we need
         self.crtk_utils = crtk.utils(self, device_namespace)
         self.crtk_utils.add_operating_state()
         self.crtk_utils.add_setpoint_cp()
-        self.crtk_utils.add_move_cp()
+        self.crtk_utils.add_servo_cv()
+        # for all examples
+        self.duration = 10 # 10 seconds
+        self.rate = 200    # aiming for 200 Hz
+        self.samples = self.duration * self.rate
 
-    def run_move_cp(self):
+    def run_servo_cv(self):
         # Not yet implemented for Galen robot
         # if not self.enable(60):
         #     print("Unable to enable the device, make sure it is connected.")
         #     return
 
-        # create a new goal starting with current position
-        start_cp = PyKDL.Frame()
-        start_cp.p = self.setpoint_cp().p
-        start_cp.M = self.setpoint_cp().M
-        goal = PyKDL.Frame()
-        goal.p = self.setpoint_cp().p
-        goal.M = self.setpoint_cp().M
-        amplitude = 20.0 # 2 centimeters
-
-        # first move
-        goal.p[0] = start_cp.p[0] + amplitude
-        goal.p[1] = start_cp.p[1] + amplitude
-        goal.p[2] = start_cp.p[2]
-        handle = self.move_cp(goal)
-        handle.wait()
-        # second move
-        goal.p[0] = start_cp.p[0] - amplitude
-        goal.p[1] = start_cp.p[1] - amplitude
-        self.move_cp(goal).wait()
-        # back to starting point
-        goal.p[0] = start_cp.p[0]
-        goal.p[1] = start_cp.p[1]
-        self.move_cp(goal).wait()
-
+        # create a new goal with constant speed
+        for i in range(self.samples):
+            vel = np.array([5.0, 0.0, 0.0, 0.0, 0.0, 0.0]) # move 5 mm/sec along x direction
+            self.servo_cv(vel)
+            rospy.sleep(1.0 / self.rate)
 
 # use the class now, i.e. main program
 if __name__ == '__main__':
@@ -73,9 +58,9 @@ if __name__ == '__main__':
         if (len(sys.argv) != 2):
             print(sys.argv[0], ' requires one argument, i.e. crtk device namespace')
         else:
-            example = crtk_move_cp_example()
+            example = crtk_servo_cv_example()
             example.configure(sys.argv[1])
-            example.run_move_cp()
+            example.run_servo_cv()
 
     except rospy.ROSInterruptException:
         pass
